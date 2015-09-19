845c9f4a1fce39807ac8fc4bab684a648ffd0f9b
Ming Liu <vmliu1@gmail.com>
2015-09-18 23:54:50 -0700

    change compile to handle user name spaced modules

--- Brocfile.js
+++ Brocfile.js
@@ -1,40 +1,60 @@
 
+var chalk = require('chalk');
 var dist = require('broccoli-dist-es6-module');
 var funnel = require('broccoli-funnel');
 var merge = require('broccoli-merge-trees');
 var path = require('path');
 
 var plugins = require('./index');
+var trees = [];
 
-module.exports = merge(plugins.map(buildPlugin));
+for (var author in plugins) {
+  trees.push(buildPlugin(author, plugins[author]));
+}
 
-function buildPlugin(name) {
-  var srcPath = path.join('lib', name);
+module.exports = merge(trees);
 
-  var srcTree = funnel(srcPath, {
+function buildPlugin(author, plugin) {
+  var globalName = 'd3.plugins.' + author + '.' + plugin;
+  var packageName = 'd3-plugins/' + author + '/' + plugin;
+
+  var pluginBasePath = path.join('lib', author, plugin);
+
+  var srcTree = funnel(pluginBasePath, {
     srcDir: '/',
-    files: [ name + '.js', 'LICENSE' ],
+    files: [ plugin + '.js', 'LICENSE' ]
   });
   var es6Tree = funnel(srcTree, {
+    srcDir: '/',
+    files: [ plugin + '.js', 'LICENSE' ],
     destDir: 'es6'
   });
 
   var distTree = dist(srcTree, {
     // the entry script, and module that becomes the global
-    main: name,
+    main: plugin,
 
     // will become window.ic.ajax with the exports from `main`
-    global: 'd3.plugins.' + name,
+    global: globalName,
 
     // the prefix for named-amd modules
-    packageName: 'd3-plugins/' + name,
+    packageName: packageName,
 
     // global output only: naive shimming, when the id 'ember' is imported,
     // substitute with `window.Ember` instead
     shim: { 'd3': 'd3' }
   });
 
+  var prefix = ' ' + chalk.green('ok') + ' ';
+
+  console.log(chalk.underline(author + '/' + plugin));
+  console.log(prefix + 'es6');
+  console.log(prefix + 'cjs');
+  console.log(prefix + 'anonymous-amd');
+  console.log(prefix + 'named-amd -> ' + packageName);
+  console.log(prefix + 'global -> ' + globalName);
+
   return funnel(merge([ es6Tree, distTree ]), {
-    destDir: name
+    destDir: path.join(author, plugin)
   });
 }
--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 
-module.exports = [
-  'adjacency-matrix',
-  'box',
-  'bullet',
-  'chernoff',
-  //cubehelix/ exports multiple
-  'fisheye',
-  'force-labels',
-  //geo/
-  'geodesic',
-  //geom/
-  //graph/
-  'hexbin',
-  'hive',
-  'horizon',
-  //interpolate-zoom/ // deprecated
-  'jsonp',
-  'keybinding',
-  'lasso',
-  'longscroll',
-  'qq',
-  'rollup',
-  'sankey',
-  //simplify/ // deprecated
-  //superformula/
-  //urlencode/
-];
+module.exports = {
+  'emeeks': 'adjacency-matrix'
+};
+//module.exports = [
+//  'adjacency-matrix',
+//  'box',
+//  'bullet',
+//  'chernoff',
+//  //cubehelix/ exports multiple
+//  'fisheye',
+//  'force-labels',
+//  //geo/
+//  'geodesic',
+//  //geom/
+//  //graph/
+//  'hexbin',
+//  'hive',
+//  'horizon',
+//  //interpolate-zoom/ // deprecated
+//  'jsonp',
+//  'keybinding',
+//  'lasso',
+//  'longscroll',
+//  'qq',
+//  'rollup',
+//  'sankey',
+//  //simplify/ // deprecated
+//  //superformula/
+//  //urlencode/
+//];
