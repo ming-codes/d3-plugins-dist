c275d6f1705af9c54b3cc66ffc2fae6107b2cd76
Ming Liu <vmliu1@gmail.com>
2015-11-18 23:15:14 -0800

    log and toTree should not be exposed as public

--- Brocfile.js
+++ Brocfile.js
@@ -1,9 +1,59 @@
 
+var path = require('path');
+var chalk = require('chalk');
+
+var dist = require('broccoli-dist-es6-module');
+var funnel = require('broccoli-funnel');
 var merge = require('broccoli-merge-trees');
 var plugins = require('./index');
 
-module.exports = merge([].concat(plugins.map(function (plugin) {
-  plugin.log();
+function makeTree(plugin) {
+  var name = plugin.name;
+  var author = plugin.author;
+  var packageName = plugin.packageName;
+  var globalName = plugin.globalName;
 
-  return plugin.toTree();
+  var pluginBasePath = plugin.basePath;
+
+  var srcTree = funnel(pluginBasePath, {
+    srcDir: '/',
+    include: [ '*.js', 'LICENSE' ]
+  });
+  var es6Tree = funnel(srcTree, {
+    srcDir: '/',
+    include: [ '*.js', 'LICENSE' ],
+    destDir: 'es6'
+  });
+
+  var distTree = dist(srcTree, {
+    // the entry script, and module that becomes the global
+    main: 'index',
+
+    // will become window.ic.ajax with the exports from `main`
+    global: globalName,
+
+    // the prefix for named-amd modules
+    packageName: packageName,
+
+    // global output only: naive shimming, when the id 'ember' is imported,
+    // substitute with `window.Ember` instead
+    shim: { 'd3': 'd3' }
+  });
+
+  return funnel(merge([ es6Tree, distTree ]), {
+    destDir: path.join(author, name)
+  });
+};
+
+module.exports = merge([].concat(plugins.map(function (plugin) {
+  var prefix = ' ' + chalk.green('ok') + ' ';
+
+  console.log(chalk.underline(plugin.author + '/' + plugin.name));
+  console.log(prefix + 'es6');
+  console.log(prefix + 'cjs');
+  console.log(prefix + 'anonymous-amd');
+  console.log(prefix + 'named-amd -> ' + plugin.packageName);
+  console.log(prefix + 'global -> ' + plugin.globalName);
+
+  return makeTree(plugin);
 })));
--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 
-var merge = require('broccoli-merge-trees');
-var chalk = require('chalk');
-var dist = require('broccoli-dist-es6-module');
-var funnel = require('broccoli-funnel');
 var path = require('path');
 
 var camelCase = require('./tests/utils').camelCase;
@@ -28,55 +24,6 @@ Plugin.fromIndex = function (index) {
   }
 
   return trees;
-};
-
-Plugin.prototype.log = function log() {
-  var prefix = ' ' + chalk.green('ok') + ' ';
-
-  console.log(chalk.underline(this.author + '/' + this.name));
-  console.log(prefix + 'es6');
-  console.log(prefix + 'cjs');
-  console.log(prefix + 'anonymous-amd');
-  console.log(prefix + 'named-amd -> ' + this.packageName);
-  console.log(prefix + 'global -> ' + this.globalName);
-};
-
-Plugin.prototype.toTree = function toTree() {
-  var plugin = this.name;
-  var author = this.author;
-  var packageName = this.packageName;
-  var globalName = this.globalName;
-
-  var pluginBasePath = this.basePath;
-
-  var srcTree = funnel(pluginBasePath, {
-    srcDir: '/',
-    include: [ '*.js', 'LICENSE' ]
-  });
-  var es6Tree = funnel(srcTree, {
-    srcDir: '/',
-    include: [ '*.js', 'LICENSE' ],
-    destDir: 'es6'
-  });
-
-  var distTree = dist(srcTree, {
-    // the entry script, and module that becomes the global
-    main: 'index',
-
-    // will become window.ic.ajax with the exports from `main`
-    global: globalName,
-
-    // the prefix for named-amd modules
-    packageName: packageName,
-
-    // global output only: naive shimming, when the id 'ember' is imported,
-    // substitute with `window.Ember` instead
-    shim: { 'd3': 'd3' }
-  });
-
-  return funnel(merge([ es6Tree, distTree ]), {
-    destDir: path.join(author, plugin)
-  });
 };
 
 module.exports = Plugin.fromIndex({
@@ -112,4 +59,3 @@ module.exports = Plugin.fromIndex({
     'topojson'
   ],
 });
-
